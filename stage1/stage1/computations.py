a322a57518d4cd66ccb5b72bebba496b48511d6f
viktorashi <ioanvictorstan@gmail.com>
2025-03-15 14:59:14 +0200

    apare la pamant ok dar nu sunt masele deloc cu sens

--- stage1/stage1/computations.py
+++ stage1/stage1/computations.py
@@ -1,4 +1,35 @@
-# e gen lista din fisier, exemplu:
+from math import sqrt
+def get_escape_velocities(data:str)->[dict]:
+    planets = parse_planets(data)
+    earth_mass = -1
+
+    for planet in planets:
+        if planet['name'] == 'Earth':
+            earth_mass = planet['mass']
+            break
+
+    for planet in planets:
+        if planet['name'] != 'Earth':
+            planet['mass'] = int ( planet['mass'] * earth_mass )
+            planet['escape_velocity'] = calculate_escape_velocity(planet['mass'], planet['diameter']/2)
+        else:
+            planet['escape_velocity'] = calculate_escape_velocity(earth_mass, planet['diameter']/2)
+
+        print(planet)
+
+
+def calculate_escape_velocity(mass, radius)-> float:
+    """
+    :param mass: of the planet in kg
+    :param radius: of the planet in km (needs converted to SI)
+    :return: its escape velocity in m/s
+    """
+    G = 6.67430 * (10 ** -11) #Newton’s gravitational constant
+    return sqrt( 2 * G * mass / (radius * (10 ** 3))  )
+
+def parse_planets(file_data:str)->[dict]:
+    """
+    # e gen lista din fisier, exemplu:
 # Mercury: diameter = 4900 km, mass = 0.06 Earths
 # Venus: diameter = 12100 km, mass = 0.82 Earths
 # Earth: diameter = 12800 km, mass = 6 * 10^24 kg
@@ -8,17 +39,10 @@
 # Uranus: diameter = 52400 km, mass = 15 Earths
 # Neptune: diameter = 48400 km, mass = 17 Earths
 # Pluto: diameter = 2450 km, mass = 0.002 Earths
-#
-def get_escape_velocities(data:str)->[dict]:
-    planets = parse_planets(data)
+    :param file_data: string cu date cum sunt scirse sus
+    :return: lista cu dict cu datele basic (alea care se pot extrage pur si simplu din Planetary_data.txt pt fiecare planeta
+    """
 
-    for planet in planets:
-        if planet['name'] == 'Earth':
-            earth_mass = planet['mass']
-            break
-
-
-def parse_planets(file_data:str)->[dict]:
     print(file_data)
     earth_mass = -1
     planets = []
@@ -32,10 +56,10 @@ def parse_planets(file_data:str)->[dict]:
             mass = eq_split[2]
 
             if 'kg' == mass[-2:]:
-                print('avem un pamantt')
                 # kg (cam multe)
-                mass = 6 * 10 ** 24  # bit cheap but does the job
+                mass = 6 * (10 ** 24)  # bit cheap but does the job
                 earth_mass = mass
+           #daca nu e pamantul dar e dupa ce s-a gasti pamantul
             elif earth_mass != -1:
                 earths_masses = mass.split(' ')[1]
                 mass = earth_mass * float(earths_masses)
@@ -49,11 +73,10 @@ def parse_planets(file_data:str)->[dict]:
                 "mass": mass
             })
 
-            print(planet_name, diameter, mass)
-
     for planet in planets:
-        planet["mass"] = planet["mass"] * earth_mass
-        if planet['name'] == 'Earth':
+        if planet['name'] != 'Earth':
+            planet["mass"] = planet["mass"] * earth_mass
+        else:
             break
 
     return planets
