c6bf040579a623a015e88d14a0509ca9014e130b
viktorashi <ioanvictorstan@gmail.com>
2025-03-16 00:38:40 +0200

    okkii merge sa se vada from planet to planet si imi valideazaa

---
+++ soft_challange/soft_challange/static/result.js
@@ -0,0 +1,17 @@
+function travelFormSubmitHandler() {
+    const travelForm = document.forms['travelForm']
+    const submitterButton = document.getElementById('travelSubmitButton')
+
+    const formData = new FormData(travelForm, submitterButton);
+
+    if (formData.get('from_planet') === formData.get('to_planet')) {
+        const travelError = document.getElementById('travelError')
+        travelError.innerText = 'The planets must be different, sillyy'
+        travelError.style.display = 'block'
+        // it's NOT safe to submit the form
+        return false;
+    }
+   // it's safe to submit the form
+    return true;
+
+}
--- soft_challange/soft_challange/views.py
+++ soft_challange/soft_challange/views.py
@@ -4,14 +4,10 @@ from utils import get_escape_velocities, get_escape_time_distance
 import os
 
 
-@app.route('/')
-def home():
-    return render_template("home.html")
-
-def file_present_valid(file_name)-> []:
+def file_present_valid(file_name) -> []:
     """
     :param file_name: The name of the file as defined in our front-end
-    :return: list: fierst element if good reposnbe or not 
+    :return: list: fierst element if good reposnbe or not
     """
 
 def save_file(file):
@@ -24,34 +20,49 @@ def save_file(file):
     request.files[file].save(filepath)
     return filepath
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
+def proccess_planet_and_rocket_data() -> [[dict], dict]:
+    """
+    Just keeping it DRY
+    :return:
+    """
+
+    rocket_data_filepath = save_file('rocket_data_file')
+    planteray_data_filepath = save_file('planetary_data_file')
+    # process the file and compute data
+    with open(planteray_data_filepath, 'r') as f:
+        planetary_data = f.read()
+    with open(rocket_data_filepath, 'r') as f:
+        rocket_data = f.read()
+    planets, rocket = get_escape_time_distance(planetary_data, rocket_data)
+    return planets, rocket
+
+@app.get('/')
+def home():
+    return render_template("home.html")
+
+@app.post('/upload')
+def upload():
     ## asta formeaza gen guardul
     ### daca vrem sa aflam chestii desrpe sistemul solar ne trebuie toate alea de dinainte,
     ### daca vrem doar despre ce face reckta noastra cand decoleaza ne trebuie alea planetare,
     ### si alea planetare sunt singurele gen 'indenpendente' sa zic asa
 
     if request.files['travel_data_file'].filename != '':
-        if not ( request.files['planetary_data_file'] or request.files['rocket_data_file'] ):
+        if not (request.files['planetary_data_file'] or request.files['rocket_data_file']):
             flash('We need both planetary and rocket data for this!')
             return redirect(url_for('home'))
 
-        #TODO to be continued lol
-        pass
+        planets, rocket = proccess_planet_and_rocket_data()
+
+        return render_template('result.html', planets=planets, rocket=rocket, travel_data=True)
 
     if request.files['rocket_data_file'].filename != '':
         if not request.files['planetary_data_file']:
             flash('We need planetary data for this as well!')
             return redirect(url_for('home'))
 
-        rocket_data_filepath = save_file('rocket_data_file')
-        planteray_data_filepath = save_file('planetary_data_file')
-        # process the file and compute data
-        with open(planteray_data_filepath, 'r') as f:
-            planetary_data = f.read()
-        with open(rocket_data_filepath, 'r') as f:
-            rocket_data = f.read()
-        planets, rocket = get_escape_time_distance(planetary_data, rocket_data)
+        planets, rocket = proccess_planet_and_rocket_data()
+
         return render_template('result.html', planets=planets, rocket=rocket)
 
     if request.files['planetary_data_file'].filename != '':
@@ -68,3 +79,13 @@ def upload_file():
     else:
         flash('Cmonnn upload *somethinggg*!!')
         return redirect(url_for('home'))
+
+@app.post('/travel')
+def travel():
+    #this will be a form with two select fields containing planet names their names are from_planet and to_planet, first check if they are different and giev a flash if not
+    if request.form['from_planet'] == request.form['to_planet']:
+        flash('These are the same planets, dummyy')
+        return redirect(url_for('upload'))
+
+    # TODO make computationes
+    return render_template("travel.html")
