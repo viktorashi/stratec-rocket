f27317c899d39dfce1d4b17b8fa746b69197b511
viktorashi <ioanvictorstan@gmail.com>
2025-03-16 02:15:11 +0200

    ok frumos misto acum tre sa-mi dau seama de ce apar negativ gen wehat

--- soft_challange/soft_challange/utils.py
+++ soft_challange/soft_challange/utils.py
@@ -1,24 +1,17 @@
 from math import sqrt
 
-def get_escape_velocities(data:str)->[dict]:
+
+def get_escape_velocities(data: str) -> [dict]:
     planets = parse_planets(data)
 
     for planet in planets:
-        planet['escape_velocity'] = calculate_escape_velocity(planet['mass'], planet['diameter']/2)
+        planet['escape_velocity'] = calculate_escape_velocity(planet['mass'], planet['diameter'] / 2)
         print(planet)
 
     return planets
 
-def calculate_escape_velocity(mass, radius)-> float:
-    """
-    :param mass: of the planet in kg
-    :param radius: of the planet in km (needs converted to SI)
-    :return: its escape velocity in m/s
-    """
-    G = 6.67430 * (10 ** -11) #Newton’s gravitational constant
-    return sqrt( 2 * G * mass / (radius * (10 ** 3))  )
 
-def get_escape_time_distance(planetary_data:str, rocket_data:str)->[dict]:
+def get_escape_time_distance(planetary_data: str, rocket_data: str) -> [dict]:
     """
     :param planetary_data: string with data about planets
     :param rocket_data: string with data about rockets
@@ -28,26 +21,142 @@ def get_escape_time_distance(planetary_data:str, rocket_data:str)->[dict]:
     rocket_data = parse_rocket(rocket_data)
 
     for planet in planets:
-        #we first need the scape velocities
-        planet['escape_velocity'] = calculate_escape_velocity(planet['mass'], planet['diameter']/2)
+        # we first need the scape velocities
+        planet['escape_velocity'] = calculate_escape_velocity(planet['mass'], planet['diameter'] / 2)
         planet['escape_time'] = calculate_escape_time(planet, rocket_data)
-        planet['escape_distance'] = calculate_escape_distance(rocket_data['acceleration'] * rocket_data['engine_count'], planet['escape_time'])
+        planet['escape_distance'] = calculate_escape_distance(rocket_data['acceleration'] * rocket_data['engine_count'],
+                                                              planet['escape_time'])
         print(planet)
 
     return planets, rocket_data
 
-def calculate_escape_time(planet :dict, rocket_data :dict)-> float:
+
+def parse_travel(travel_data: str, planetary_data: [dict]) -> [dict]:
+    """
+    Obtains list of planets but with orbital radii now
+
+    :param travel_data: file of the form
+        Mercury: period = 88 days, orbital radius = 0.39 AU
+        Venus: period = 225 days, orbital radius = 0.72 AU
+        Earth: period = 365 days, orbital radius = 1 AU
+        Mars: period = 687 days, orbital radius = 1.52 AU
+        Jupiter: period = 4329 days, orbital radius = 5.2 AU
+        Saturn: period = 10753 days, orbital radius = 9.54 AU
+        Uranus: period = 30660 days, orbital radius = 19.18 AU
+        Neptune: period = 60148 days, orbital radius = 30.06 AU
+        Pluto: period = 90560 days, orbital radius = 39.6 AU
+
+    :param planetary_data:
+    :return: list of planets but with orbital radii now
+    """
+
+    for planet in planetary_data:
+        for line in travel_data.split('\n'):
+            if planet['name'] in line:
+                planet['orbital_radius'] = float(line.split('=')[2].split(' ')[1])
+                break
+
+    return planetary_data
+
+
+def get_travel_data(travel_data: str, planetary_data: [dict], rocket_data: dict, from_planet: str,
+                    to_planet: str) -> dict:
+    """
+    :param travel_data: string from traveel.txt of the form:
+        Mercury: period = 88 days, orbital radius = 0.39 AU
+        Venus: period = 225 days, orbital radius = 0.72 AU
+        Earth: period = 365 days, orbital radius = 1 AU
+        Mars: period = 687 days, orbital radius = 1.52 AU
+        Jupiter: period = 4329 days, orbital radius = 5.2 AU
+        Saturn: period = 10753 days, orbital radius = 9.54 AU
+        Uranus: period = 30660 days, orbital radius = 19.18 AU
+        Neptune: period = 60148 days, orbital radius = 30.06 AU
+        Pluto: period = 90560 days, orbital radius = 39.6 AU
+
+    :param planetary_data: returned by get_escape_time_distance
+    :param rocket_data: returned by get_escape_time_distance
+    :param from_planet: from the form
+    :param to_planet:  from the form
+    :return: the 6 requirements of the problem statement in a dict
+
+     // take the ecape velocities of the two planets, and set the max of them to be the cruising velocity
+    'escape_time' 1. the time it takes to reach the cruising velocity from the start planet : planetary_data['max_velocity_planet']['escape_time']
+    'escape_distance' 2. the distance from the surface when it reaches it :  planetary_data['max_velocity_planet']['escape_distance']
+    'cruise_time' 3. Cruise time : ( distance_between_planets_centers - 2 * escape_distance - radius_from_planet -radius_to_planet ) / cruising_velocity
+    'escape_distance' 4. Distance at which it starts deccelarting (from planet's surface) : SAME AS 2. !! ( nu inteleg eu ceva? gen e intrebare capana pusa inca o data?)
+    'escape_time' 5. Time to deccelerate to 0 : SAME AS 1. !!!!1 (again, sunt eu prost?)
+    'total_travel_time' 6. Total travel time : ( distance_between_planets_centers - radius_from_planet - radius_to_planet ) / cruising_velocity
+    # ( in days, hours, minutes, seconds)
+
+    """
+
+    planets = parse_travel(travel_data, planetary_data)
+
+    from_planet_data = -1
+    to_planet_data = -1
+    travel_results = {}
+
+    for planet in planets:
+        if planet['name'] == from_planet:
+            from_planet_data = planet
+        elif planet['name'] == to_planet:
+            to_planet_data = planet
+        elif from_planet_data != -1 and to_planet_data != -1:
+            break
+
+    if from_planet_data['escape_velocity'] > to_planet_data['escape_velocity']:
+        cruising_velocity = from_planet_data['escape_velocity']
+        escape_distanace = from_planet_data['escape_distance']
+    else:
+        cruising_velocity = to_planet_data['escape_velocity']
+        escape_distanace = to_planet_data['escape_distance']
+
+    travel_results['escape_time'] = from_planet_data['escape_time']
+    travel_results['escape_distance'] = from_planet_data['escape_distance']
+
+    distance_between_planet_centers = abs(from_planet_data['orbital_radius'] - to_planet_data['orbital_radius'])
+
+    # long boi
+    travel_results['cruise_time'] = calculate_cruise_time(distance_between_planet_centers, escape_distanace,
+                                                          from_planet_data['diameter'] / 2,
+                                                          to_planet_data['diameter'] / 2, cruising_velocity)
+
+    travel_results['total_travel_time'] = (distance_between_planet_centers - from_planet_data['diameter'] / 2 -
+                                           to_planet_data['diameter'] / 2) / cruising_velocity
+
+    return travel_results
+
+
+def calculate_escape_velocity(mass, radius) -> float:
+    """
+    :param mass: of the planet in kg
+    :param radius: of the planet in km (needs converted to SI)
+    :return: its escape velocity in m/s
+    """
+    G = 6.67430 * (10 ** -11)  # Newton’s gravitational constant
+    return sqrt(2 * G * mass / (radius * (10 ** 3)))
+
+
+def calculate_escape_time(planet: dict, rocket_data: dict) -> float:
     """
     :param planet: Dict with data about the planet of the type {"name": "Earth", "diameter": 12800, "mass": 6 * (10 ** 24)}
     :param rocket_data: dict with no. of engines and each engine's acceleration {"engine_count": 4, "acceleration": 10}
     :return: escape time in seconds : velocity / ( engine_count * acceleration_per_engine)
     """
-    return planet['escape_velocity'] / ( rocket_data['engine_count'] * rocket_data['acceleration'] )
+    return planet['escape_velocity'] / (rocket_data['engine_count'] * rocket_data['acceleration'])
 
-def calculate_escape_distance(acceleration, escape_time)-> float:
-    return   ( acceleration * escape_time**2 ) / 2
 
-def parse_rocket(file_data:str)->dict:
+def calculate_escape_distance(acceleration, escape_time) -> float:
+    return (acceleration * escape_time ** 2) / 2
+
+
+def calculate_cruise_time(distance_between_planets_centers, escape_distance, radius_from_planet, radius_to_planet,
+                          cruising_velocity) -> float:
+    return (
+            distance_between_planets_centers - 2 * escape_distance - radius_from_planet - radius_to_planet) / cruising_velocity
+
+
+def parse_rocket(file_data: str) -> dict:
     """
     :param file_data: Contents of the form:
     Number of rocket engines: 4
@@ -57,9 +166,9 @@ def parse_rocket(file_data:str)->dict:
     """
 
     rocket = {}
-    #there's only two lines lmao
+    # there's only two lines lmao
     file_lines = file_data.split('\n')[:-1]
-    [no_engines,acceleration_per_engine] = file_lines
+    [no_engines, acceleration_per_engine] = file_lines
 
     no_engines = int(no_engines.split(': ')[1])
     acceleration_per_engine = float(acceleration_per_engine.split(': ')[1].split(' ')[0])
@@ -69,7 +178,8 @@ def parse_rocket(file_data:str)->dict:
 
     return rocket
 
-def parse_planets(file_data:str)->[dict]:
+
+def parse_planets(file_data: str) -> [dict]:
     """
     # e gen lista din fisier, exemplu:
 # Mercury: diameter = 4900 km, mass = 0.06 Earths
@@ -106,11 +216,11 @@ def parse_planets(file_data:str)->[dict]:
                 # kg (cam multe)
                 mass = 6 * (10 ** 24)  # a bit cheap but does the job
                 earth_mass = mass
-           #daca nu e pamantul dar e dupa ce s-a gasti pamantul
+            # daca nu e pamantul dar e dupa ce s-a gasti pamantul
             elif earth_mass != -1:
                 earths_masses = mass.split(' ')[1]
                 mass = earth_mass * float(earths_masses)
-            #the ones before earth came up
+            # the ones before earth came up
             else:
                 mass = float(mass.split(' ')[1])
 
@@ -127,4 +237,3 @@ def parse_planets(file_data:str)->[dict]:
             break
 
     return planets
-
--- soft_challange/soft_challange/views.py
+++ soft_challange/soft_challange/views.py
@@ -1,6 +1,6 @@
 from flask import request, redirect, url_for, render_template, flash
 from soft_challange import app
-from utils import get_escape_velocities, get_escape_time_distance
+from utils import get_escape_velocities, get_escape_time_distance, get_travel_data
 import os
 
 
@@ -10,15 +10,18 @@ def file_present_valid(file_name) -> []:
     :return: list: fierst element if good reposnbe or not
     """
 
-def save_file(file):
+
+def save_file(file, new_name):
     """
     :param file: The file to be saved
+    :param new_name: The name to be saved as
     :return: The path to the saved file
     """
-    filename = request.files[file].filename
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], new_name)
+    #ignore this warning lmao
     request.files[file].save(filepath)
     return filepath
+
 
 def proccess_planet_and_rocket_data() -> [[dict], dict]:
     """
@@ -26,8 +29,8 @@ def proccess_planet_and_rocket_data() -> [[dict], dict]:
     :return:
     """
 
-    rocket_data_filepath = save_file('rocket_data_file')
-    planteray_data_filepath = save_file('planetary_data_file')
+    rocket_data_filepath = save_file('rocket_data_file', 'rocket_data_file.txt')
+    planteray_data_filepath = save_file('planetary_data_file', 'planetary_data_file.txt')
     # process the file and compute data
     with open(planteray_data_filepath, 'r') as f:
         planetary_data = f.read()
@@ -36,9 +39,11 @@ def proccess_planet_and_rocket_data() -> [[dict], dict]:
     planets, rocket = get_escape_time_distance(planetary_data, rocket_data)
     return planets, rocket
 
+
 @app.get('/')
 def home():
     return render_template("home.html")
+
 
 @app.post('/upload')
 def upload():
@@ -53,6 +58,7 @@ def upload():
             return redirect(url_for('home'))
 
         planets, rocket = proccess_planet_and_rocket_data()
+        save_file('travel_data_file', 'travel_data_file.txt')
 
         return render_template('result.html', planets=planets, rocket=rocket, travel_data=True)
 
@@ -80,12 +86,32 @@ def upload():
         flash('Cmonnn upload *somethinggg*!!')
         return redirect(url_for('home'))
 
+
 @app.post('/travel')
 def travel():
-    #this will be a form with two select fields containing planet names their names are from_planet and to_planet, first check if they are different and giev a flash if not
-    if request.form['from_planet'] == request.form['to_planet']:
+    # this will be a form with two select fields containing planet names their names are from_planet and to_planet, first check if they are different and giev a flash if not
+    from_planet = request.form['from_planet']
+    to_planet = request.form['to_planet']
+
+    if from_planet == to_planet:
         flash('These are the same planets, dummyy')
         return redirect(url_for('upload'))
 
-    # TODO make computationes
-    return render_template("travel.html")
+    travel_data_filepath = os.path.join(app.config['UPLOAD_FOLDER'], 'travel_data_file.txt')
+
+    with open(travel_data_filepath, 'r') as f:
+        travel_data = f.read()
+
+    planetary_data_filepath = os.path.join(app.config['UPLOAD_FOLDER'], 'planetary_data_file.txt')
+    rocket_data_filepath = os.path.join(app.config['UPLOAD_FOLDER'], 'rocket_data_file.txt')
+
+    # process the file and compute data
+    with open(planetary_data_filepath, 'r') as f:
+        planetary_data = f.read()
+    with open(rocket_data_filepath, 'r') as f:
+        rocket_data = f.read()
+    planets, rocket = get_escape_time_distance(planetary_data, rocket_data)
+
+    travel_results = get_travel_data(travel_data, planets, rocket, from_planet, to_planet)
+
+    return render_template("travel.html", travel=travel_results)
